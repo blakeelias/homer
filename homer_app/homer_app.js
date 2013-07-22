e8109674a2bcddb629a1de92c46b682ec5d9678a
Blake Elias <blakeelias@gmail.com>
2013-07-21 22:39:54 -0700

    Add easy, medium and hard buttons; update last seen time and easiness factor (EF looks wrong though)

--- homer_app/homer_app.js
+++ homer_app/homer_app.js
@@ -1,5 +1,5 @@
 Cards = new Meteor.Collection("cards");
-
+CardViews = new Meteor.Collection("card_views");
 
 if (Meteor.isClient) {
   Template.list.greeting = function () {
@@ -17,10 +17,16 @@ if (Meteor.isClient) {
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
     },
-    'click input.right': function() {
-        Cards.update(Session.get('selected_card'), {$set: {'last_seen' : new Date().UTC()}});
+    'click input.easy': function() {
+        var currentCard = Session.get('selected_card');
+        currentEasiness = Cards.findOne(currentCard).easiness;
+        Cards.update(currentCard, {$set:
+            {'last_seen_millis' : (new Date()).getTime(),
+             'easiness': newEasinessFactor(currentEasiness, 3)}}
+        );
         var card = Cards.findOne(Session.get("selected_card"));
-        console.log(card.last_seen);
+        console.log(card.last_seen_millis);
+        console.log(card.easiness);
     }
 
   });
@@ -46,17 +52,21 @@ function newEasinessFactor(easinessFactor, quality) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
       if (Cards.find().count() === 0) {
-        Cards.insert({"question": "What is 2 + 2",
+        Cards.insert({"_id": 1,
+                      "question": "What is 2 + 2",
                       "answer": "4",
-                      "last_seen": -1,
-                      "difficulty": 2.5,
-                      "next_scheduled": (new Date()).UTC()
+                      "last_seen_millis": -1,
+                      "easiness": 2.5,
+                      "next_scheduled_millis": (new Date()).getTime(),
+                      "history": []
         });
-        Cards.insert({"question": "Who was the first US president?",
+        Cards.insert({"_id": 2,
+                      "question": "Who was the first US president?",
                       "answer": "George Washington",
-                      "last_seen": -1,
-                      "difficulty": 2.5,
-                      "next_scheduled": (new Date()).UTC()
+                      "last_seen_millis": -1,
+                      "easiness": 2.5,
+                      "next_scheduled_millis": (new Date()).getTime(),
+                      "history": []
         });
       }
   });
