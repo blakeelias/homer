4edb860a7380e40fc7b22bfb4a550c240f853700
Blake Elias <blakeelias@gmail.com>
2015-01-09 19:16:50 -0500

    Add a sample question when 'Import button is clicked in pop-up form'

--- homer_tasks/import-form.js
+++ homer_tasks/import-form.js
@@ -47,26 +47,7 @@ showInputForm = function() {
 			}
  
 			function addUser() {
-				var valid = true;
-				allFields.removeClass( "ui-state-error" );
- 
-				valid = valid && checkLength( name, "username", 3, 16 );
-				valid = valid && checkLength( email, "email", 6, 80 );
-				valid = valid && checkLength( password, "password", 5, 16 );
- 
-				valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
-				valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
-				valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
- 
-				if ( valid ) {
-					$( "#users tbody" ).append( "<tr>" +
-						"<td>" + name.val() + "</td>" +
-						"<td>" + email.val() + "</td>" +
-						"<td>" + password.val() + "</td>" +
-					"</tr>" );
-					dialog.dialog( "close" );
-				}
-				return valid;
+			  Meteor.call('insertCard', {'question': 'import test'});
 			}
  
 			dialog = $( "#dialog-form" ).dialog({
@@ -96,3 +77,9 @@ showInputForm = function() {
 			});
 		});
 }
+
+Meteor.methods({
+  insertCard: function (card) {
+    Cards.insert(card);
+  }
+});
