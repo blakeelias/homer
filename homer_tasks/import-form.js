01c376d6ef008c073cc6b9aacf7364e623db0f5d
Blake Elias <blakeelias@gmail.com>
2015-01-09 19:33:59 -0500

    Remove extraneous code left-over from add-user example; rename text field ID to importData

--- homer_tasks/import-form.js
+++ homer_tasks/import-form.js
@@ -8,43 +8,7 @@ showInputForm = function() {
 		$(function() {
 			var dialog, form,
  
-				// From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
-				emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-				name = $( "#name" ),
-				email = $( "#email" ),
-				password = $( "#password" ),
-				allFields = $( [] ).add( name ).add( email ).add( password ),
-				tips = $( ".validateTips" );
- 
-			function updateTips( t ) {
-				tips
-					.text( t )
-					.addClass( "ui-state-highlight" );
-				setTimeout(function() {
-					tips.removeClass( "ui-state-highlight", 1500 );
-				}, 500 );
-			}
- 
-			function checkLength( o, n, min, max ) {
-				if ( o.val().length > max || o.val().length < min ) {
-					o.addClass( "ui-state-error" );
-					updateTips( "Length of " + n + " must be between " +
-						min + " and " + max + "." );
-					return false;
-				} else {
-					return true;
-				}
-			}
- 
-			function checkRegexp( o, regexp, n ) {
-				if ( !( regexp.test( o.val() ) ) ) {
-					o.addClass( "ui-state-error" );
-					updateTips( n );
-					return false;
-				} else {
-					return true;
-				}
-			}
+				importDataField = $( "#importData" );
  
 			function addUser() {
 			  Meteor.call('insertCard', {'question': 'import test'});
