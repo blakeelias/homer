f743d8edde22d852098524b0b2d806bc0b1d641d
Blake Elias <blakeelias@gmail.com>
2015-01-28 17:24:15 -0500

    Show and update progress bar for learning and reviewing

--- homer_tasks/homer_tasks.js
+++ homer_tasks/homer_tasks.js
@@ -10,8 +10,7 @@ if (Meteor.isClient) {
   
   Session.set("learning", false);
   Session.set("categoryToReview", null);
-  Session.set('numCardsSeen', 0);
-  Session.set('numCardsTotal', 0);
+  updateProgressBar(0, 0);
 
   Template.body.greeting = function () {
     return "Click a question below to view its answer.";
@@ -144,14 +143,7 @@ if (Meteor.isClient) {
       console.log("clicked learn button");
       Session.set("learning", true);
       Session.set("categoryToReview", this.name);
-      Session.set('numCardsTotal', Cards.find({
-        tags: this.name,
-        userId: { $exists: false }
-      }).count());
-      Session.set('numCardsSeen', Cards.find({
-        tags: this.name,
-        userId: Meteor.userId()
-      }).count());
+      updateProgressBarLearning();
     },
     'click button.review': function() {
   		console.log("clicked review button");
@@ -209,8 +201,7 @@ function reviewAll() {
   console.log("reviewing all cards due today");
   Session.set("learning", false);
   Session.set("categoryToReview", null);
-  Session.set('numCardsSeen', 0);
-  Session.set('numCardsTotal', cardsDueToday().length);
+  updateProgressBar(0, cardsDueToday().length);
 
   // maybe?
   // Session.set("categoryToBrowse", null);
@@ -235,8 +226,9 @@ function answerCard(cardReference, response) {
     } else {
       updateCard(cardReference, response);
     }
-    if (isCorrect(response)) {
-      Session.set('numCardsSeen', Session.get('numCardsTotal') - cardsDueToday().length);
+    if (isCorrect(response) || Session.get('learning')) {
+      incrementProgressBar();
+      //updateProgressBarReviewing(Session.get('numCardsTotal') - cardsDueToday().length);
     }
 }
 
@@ -352,6 +344,29 @@ function getUserCard(parentCardId) {
 		'userId': Meteor.user()._id
 	});
 }
+
+function incrementProgressBar() {
+  Session.set('numCardsSeen', Session.get('numCardsSeen') + 1)
+}
+
+function updateProgressBarLearning() {
+  updateProgressBar(
+    Cards.find({
+      tags: Session.get('categoryToReview'),
+      userId: Meteor.userId()
+    }).count(),
+    Cards.find({
+      tags: Session.get('categoryToReview'),
+      userId: { $exists: false }
+    }).count()
+  );
+}
+
+function updateProgressBar(numSeen, numTotal) {
+  Session.set('numCardsSeen', numSeen);
+  Session.set('numCardsTotal', numTotal);
+}
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
       if (Cards.find().count() === 0) {
